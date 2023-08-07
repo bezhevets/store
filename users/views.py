4bd415bc19a0b978773a638ab1d3fe84564a9693
Maksym Bezhevets <begik4@gmail.com>
2023-08-08 01:26:36 +0300

    Added login view and customized login form, updated login.html

--- users/views.py
+++ users/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+
 
 def login(request):
-    return render(request, "registration/login.html")
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            print(password)
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("products:index"))
+    else:
+        form = UserLoginForm()
+    context = {"form": form}
+    return render(request, "registration/login.html", context=context)
 
 
 def registration(request):
